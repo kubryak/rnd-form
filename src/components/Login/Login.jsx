e69f848256f396bf4f9018d603245582e39ea3a4
kubryak <al.lcn@yandex.ru>
2023-11-17 17:33:03 +0500

    feat: добавить стили кнопке, реализовать валидацию

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import './Login.css'
 import Logo from '../Logo/Logo'
+import { useFormAndValidation } from '../../hooks/useFormAndValidation'
 
 export default function Login() {
+  const { values, handleChange, errors, isValid, setIsValid } = useFormAndValidation();
+
+  const { email, password } = values;
+
+  useEffect(() => {
+    if (!email && !password) {
+      setIsValid(false)
+    }
+  }, [email, password])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // onLogin();
+  }
+
   return (
     <main>
       <section className='authorization'>
@@ -11,7 +27,7 @@ export default function Login() {
           <Logo />
         </div>
         <h1 className='authorization__title'>Рады видеть!</h1>
-        <form className='authorization__form'>
+        <form className='authorization__form' onSubmit={handleSubmit}>
           <div className='authorization__input-container'>
             <label className='authorization__input-label'>Email</label>
             <input
@@ -20,8 +36,13 @@ export default function Login() {
               name='email'
               required
               placeholder='Email'
+              value={email || ''}
+              onChange={handleChange}
+              pattern="[^@\s]+@[^@\s]+\.[^@\s]+"
             />
-            <span className='input-error input-error_active'></span>
+            <span className='input-error input-error_active'>
+              {errors.email}
+            </span>
           </div>
           <div className='authorization__input-container'>
             <label className='authorization__input-label'>Пароль</label>
@@ -31,13 +52,19 @@ export default function Login() {
               name='password'
               required
               placeholder='Пароль'
+              minLength='3'
+              maxLength='25'
+              value={password || ''}
+              onChange={handleChange}
             />
-            <span className='input-error input-error_active'></span>
+            <span className='input-error input-error_active'>
+              {errors.password}
+            </span>
           </div>
+          <p className='authorization__forgot-text'><Link className='authorization__link'>Забыли пароль?</Link></p>
           <div className='authorization__buttons'>
-            <button type='submit' className='authorization__button'>Войти</button>
-            <p className='authorization__text'>Еще не зарегистрированы?
-              <Link>Регистрация</Link>
+            <button type='submit' className={!isValid ? 'authorization__button authorization__button_type_disabled' : 'authorization__button authorization__button_type_active'} disabled={!isValid}>Войти</button>
+            <p className='authorization__text'>Еще не зарегистрированы? <Link className='authorization__link authorization__link_type_registration'>Регистрация</Link>
             </p>
           </div>
         </form>
