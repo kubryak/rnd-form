38f4c9c44e086566b6ff437bec2bdea3600c4587
kubryak <al.lcn@yandex.ru>
2023-11-18 13:07:11 +0500

    fix: удалить комментарии из кода api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,16 +1,14 @@
 export const fetchData = async () => {
   try {
-    // Мок-запрос с задержкой в 1 секунду (1000 миллисекунд)
     const response = await new Promise(resolve => {
       setTimeout(() => {
         resolve({
           status: 200,
           json: () => Promise.resolve({ data: 'mocked response' }),
         });
-      }, 1000); // Задержка в 1 секунду
+      }, 1000);
     });
 
-    // Обработка успешного ответа
     const data = await response.json();
     console.log('Запрос выполнен успешно:', data);
   } catch (error) {
